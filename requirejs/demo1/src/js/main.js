445cd6a47f827787452f7195eed30ee730db5206
zhongxia245 <zhongxia245@sina.com>
2016-01-05 10:39:02 +0800

    RequireJS中默认存在 module 的模块名

--- requirejs/demo1/src/js/main.js
+++ requirejs/demo1/src/js/main.js
@@ -1,16 +1,16 @@
 console.log("load success...");
 require.config({
     /*
-    	啥，你的JS文件都在某一个路径下，你不想在指定paths 的时候，每个都加上那个前缀
-    	那好吧，
-    	你设置baseUrl 属性，这样你就不用每次都加上那个 一样的 前缀目录名了
+        啥，你的JS文件都在某一个路径下，你不想在指定paths 的时候，每个都加上那个前缀
+        那好吧，
+        你设置baseUrl 属性，这样你就不用每次都加上那个 一样的 前缀目录名了
      */
     baseUrl: '',
     /*
-    	shim ： 处理，如果不是AMD规范的文档，如何处理
+        shim ： 处理，如果不是AMD规范的文档，如何处理
      */
     shim: {
-        module: {
+        module1: {
             exports: 'module1'
         },
         test: {
@@ -31,18 +31,18 @@ require.config({
         }
     },
     /*
-    	require的文件，默认是和 当前JS文件(main.js) 是同一目录下的
-    	但是这种情况是很少的，毕竟JS文件一多，不可能放一起，而是会分类一下
-    	所以需要配置路径，指定到特定的目录下
+        require的文件，默认是和 当前JS文件(main.js) 是同一目录下的
+        但是这种情况是很少的，毕竟JS文件一多，不可能放一起，而是会分类一下
+        所以需要配置路径，指定到特定的目录下
      */
     paths: {　　　
         "require": "/node_modules/requirejs/require",
         "easyui": "/node_modules/easyui/jquery.easyui.min",
         "jquery": "/node_modules/jquery/dist/jquery.min",
-        "myjquery": "/node_modules/jquery/dist/jquery.min", 
+        "myjquery": "/node_modules/jquery/dist/jquery.min",
         "underscore": "/node_modules/underscore/underscore-min",
         "math": "/src/js/math",
-        "module": "/src/js/module1",
+        "module1": "/src/js/module1",
         "test": "/src/js/test",
         "say": "/src/js/say"
     }　　
@@ -63,20 +63,29 @@ require.config({
 // });
 
 /*
-	easyui  和 say 模块都依赖 jquery, 但是requirejs 内部会去判断，不会去请求两次
+    easyui  和 say 模块都依赖 jquery, 但是requirejs 内部会去判断，不会去请求两次
  */
 // require(['easyui', 'say'], function(easyui) {
 //     console.log($, easyui);
-// })
+// });
 
 /*
-	如果这里用 myjquery,下面回调方法就获取不到 $ 对象【有主模块和无主模块的理解】
-	jquery中，已经是按照AMD的规范来的，definde('jquery',[],function(){})
-	解决方案：快速理解RequireJs 【http://www.tuicool.com/articles/jam2Anv】
-	注意一个问题：依赖的顺序和回调函数的参数必须是同样的顺序
-	把jquery的值赋值到 math[这里是获取不到$的]， math的值赋值到 $
+    如果这里用 myjquery,下面回调方法就获取不到 $ 对象【有主模块和无主模块的理解】
+    jquery中，已经是按照AMD的规范来的，definde('jquery',[],function(){})
+    解决方案：快速理解RequireJs 【http://www.tuicool.com/articles/jam2Anv】
+    注意一个问题：依赖的顺序和回调函数的参数必须是同样的顺序
+    把jquery的值赋值到 math[这里是获取不到$的]， math的值赋值到 $
  */
-require(['myjquery','underscore','math'], function(math,_,$) {
-	console.log(arguments);
-    console.log(math,jq,$);
+// require(['myjquery', 'underscore', 'math'], function(math, _, $) {
+//     console.log(arguments);
+//     console.log(math, _, $);
+// });
+
+/**
+ *  注意，名称 module 默认是一个requireJs中的模块
+ *  所以不要把模块的别名取成module，否则读取回来的对象是错误的，不是你想要的
+ */
+require(['module1','module'], function(module1,module) {
+    console.log(module1);
+    console.log(module);
 })
